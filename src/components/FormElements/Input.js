718d58cf1963e64a088f6e7c5f1d6b1344c2efe1
Jaichyi Chen <jaichyichen@gmail.com>
2022-10-28 12:15:04 -0400

    finished add place page front-end, need to implement backend for to complete

---
+++ src/components/FormElements/Input.js
@@ -0,0 +1,91 @@
+import React, { useReducer, useEffect } from "react";
+import "./Input.css";
+import { validate } from "../../Utils/validators";
+//useReducer setup
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE":
+      return {
+        ...state,
+        value: action.value,
+        isValid: validate(action.value, action.validators),
+      };
+    case "TOUCH":
+      return {
+        ...state,
+        isFocused: true,
+      };
+    default:
+      return state;
+  }
+};
+const Input = (props) => {
+  const {
+    type,
+    label,
+    validators,
+    id,
+    element,
+    placeholder,
+    rows,
+    onInput,
+    errorMessage,
+  } = props;
+  //initial state
+  const [state, dispatch] = useReducer(reducer, {
+    value: "",
+    isFocused: false,
+    isValid: false,
+  });
+
+  useEffect(() => {
+    onInput(id, state.value, state.isValid);
+  }, [id, state.value, state.isValid, onInput]);
+
+  const changeHandler = (e) => {
+    dispatch({
+      type: "CHANGE",
+      value: e.target.value,
+      validators: validators,
+    });
+  };
+  const focusHandler = () => {
+    dispatch({
+      type: "TOUCH",
+    });
+  };
+
+  const textInput =
+    element === "input" ? (
+      <input
+        id={id}
+        type={type}
+        placeholder={placeholder}
+        value={state.value}
+        onBlur={focusHandler}
+        onChange={changeHandler}
+      ></input>
+    ) : (
+      <textarea
+        id={id}
+        row={rows || 3}
+        value={state.value}
+        onBlur={focusHandler}
+        onChange={changeHandler}
+      ></textarea>
+    );
+
+  return (
+    <div
+      className={`form-control ${
+        !state.isValid && state.isFocused && "form-control--invalid"
+      }`}
+    >
+      <label htmlFor={id}>{label}</label>
+      {textInput}
+      {!state.isValid && state.isFocused && <p>{errorMessage}</p>}
+    </div>
+  );
+};
+
+export default Input;
--- src/pages/NewPlace.js
+++ src/pages/NewPlace.js
@@ -1,7 +1,97 @@
-import React from "react";
+import React, { useCallback, useReducer } from "react";
+import "./Newplace.css";
+import Input from "../components/FormElements/Input";
+import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from "../Utils/validators";
+import Button from "../components/FormElements/Button";
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case "INPUT_CHANGE":
+      let formIsValid = true;
+      //updating formIsValid based on the input that is being changed
+      for (const inputId in state.inputs) {
+        if (inputId === action.InputId) {
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+      //update the state in NewPlace based on the state in Input.js
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.inputId]: { value: action.value, isValid: action.isValid },
+        },
+        isValid: formIsValid,
+      };
+    default:
+      return state;
+  }
+};
 
+//useCallback to prevent infinite rerender, this function is used as dependency in input.js
 const NewPlace = () => {
-  return <div>NewPlace</div>;
+  const [state, dispatch] = useReducer(formReducer, {
+    inputs: {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      },
+    },
+    isValid: false,
+  });
+  const inputHandler = useCallback((id, value, isValid) => {
+    dispatch({
+      type: "INPUT_CHANGE",
+      value: value,
+      isValid: isValid,
+      inputId: id,
+    });
+  }, []);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    // todo
+    // send to server
+    console.log(state.inputs);
+  };
+
+  return (
+    <form className="place-form" onSubmit={submitHandler}>
+      <Input
+        id="title"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        element="input"
+        onInput={inputHandler}
+        errorMessage="Please enter a valid title."
+      ></Input>
+      <Input
+        id="description"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        element="textarea"
+        onInput={inputHandler}
+        errorMessage="Please enter a valid description of minimum 5 characters."
+      ></Input>
+      <Input
+        id="address"
+        label="Address"
+        validators={[VALIDATOR_REQUIRE()]}
+        element="input"
+        onInput={inputHandler}
+        errorMessage="Please enter a valid address."
+      ></Input>
+      <Button type="submit" disabled={!state.isValid}>
+        Add Place
+      </Button>
+    </form>
+  );
 };
 
 export default NewPlace;
